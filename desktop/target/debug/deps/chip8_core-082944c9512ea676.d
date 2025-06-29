f2f5b13d72282da83bbcd3b04bf88a5da7f0448c
Jayesh Puri <jayeshkpuri@gmail.com>
2025-06-29 21:08:52 +0530

    linked both chip8_core and frontend

---
+++ desktop/target/debug/deps/chip8_core-082944c9512ea676.d
@@ -0,0 +1,7 @@
+/home/jayesh-puri/code/CSOC/Chip8_emulation/desktop/target/debug/deps/chip8_core-082944c9512ea676.d: /home/jayesh-puri/code/CSOC/Chip8_emulation/chip8_core/src/lib.rs
+
+/home/jayesh-puri/code/CSOC/Chip8_emulation/desktop/target/debug/deps/libchip8_core-082944c9512ea676.rlib: /home/jayesh-puri/code/CSOC/Chip8_emulation/chip8_core/src/lib.rs
+
+/home/jayesh-puri/code/CSOC/Chip8_emulation/desktop/target/debug/deps/libchip8_core-082944c9512ea676.rmeta: /home/jayesh-puri/code/CSOC/Chip8_emulation/chip8_core/src/lib.rs
+
+/home/jayesh-puri/code/CSOC/Chip8_emulation/chip8_core/src/lib.rs:
