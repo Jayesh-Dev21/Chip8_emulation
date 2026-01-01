752cc068ec69746bc4e2b0d8b29397d66f543b5c
Jayesh Puri <jayeshkpuri@gmail.com>
2026-01-01 23:17:19 +0530

    read rom to _data, header sorted

---
+++ cpp/src/headers/helper.hpp
@@ -0,0 +1,7 @@
+#ifndef HELPER_HPP
+#define HELPER_HPP
+
+#include "headers.hpp"
+std::vector <unsigned char> read_file_helper(char const* filepath);
+
+#endif 
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,18 +1,13 @@
-#include <iostream>
-
-void call_func(const char* rom){
-    std::cout << "Calling game init for: " << rom << std::endl;
-}
+#include "headers/helper.hpp"
 
 int main(int argc, char* argv[]){
-    
-    if (1 == argc){
+    if (1 == argc){         
         std::cout << "Please enter the path/to/the/rom" << std::endl;
     } 
 
     else { // ignore all argv[:2]
         try{
-            call_func(argv[1]);
+            const auto _data = read_file_helper(argv[1]);
         } catch(const std::exception& e){
             std::cerr << "Error: " << e.what() << std::endl;
             return 1;
