df75d9c1fa711e3a3c45e2686b5b24d46027cb73
Jayesh Puri <jayeshkpuri@gmail.com>
2026-01-01 20:30:20 +0530

    try-catch main

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
-int main(int argc, char* argv[]) {
+void call_func(const char* rom){
+    std::cout << "Calling game init for: " << rom << std::endl;
+}
+
+int main(int argc, char* argv[]){
     
-    std::cout << "Hello, World!" << std::endl;
+    if (1 == argc){
+        std::cout << "Please enter the name of the rom file" << std::endl;
+    } 
+
+    else { // ignore all argv[:2]
+        try{
+            call_func(argv[1]);
+        } catch(const std::exception& e){
+            std::cerr << "Error: " << e.what() << std::endl;
+            return 1;
+        }
+    }
+
+    // std::cout << "Hello, World!" << std::endl; -- Test 1
     return 0;
 }
